7570e551a9fdda239c886209681d0c0aedc29c14
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-02-29 13:57:42 +0100

    Update API for personalized results (#103)
    * Update API for personalized results
    
    * Allow non-signed in users to init
    
    * Use entry within coordinator to get site_reference

--- custom_components/frank_energie/__init__.py
+++ custom_components/frank_energie/__init__.py
@@ -20,6 +20,30 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if entry.unique_id is None or entry.unique_id == "frank_energie_component":
         hass.config_entries.async_update_entry(entry, unique_id=str("frank_energie"))
 
+    # Select site-reference, or find first one that has status 'IN_DELIVERY' if not set
+    if entry.data.get("site_reference") is None and entry.data.get(CONF_ACCESS_TOKEN) is not None:
+        api = FrankEnergie(
+            clientsession=async_get_clientsession(hass),
+            auth_token=entry.data.get(CONF_ACCESS_TOKEN, None),
+            refresh_token=entry.data.get(CONF_TOKEN, None),
+        )
+        me = await api.me()
+
+        # filter out all sites that are not in delivery
+        me.deliverySites = [site for site in me.deliverySites if site.status == "IN_DELIVERY"]
+
+        if len(me.deliverySites) == 0:
+            raise Exception("No suitable sites found for this account")
+
+        site = me.deliverySites[0]
+        hass.config_entries.async_update_entry(entry, data={**entry.data, "site_reference": site.reference})
+
+        # Update title
+        title = f"{site.address_street} {site.address_houseNumber}"
+        if site.address_houseNumberAddition is not None:
+            title += f" {site.address_houseNumberAddition}"
+        hass.config_entries.async_update_entry(entry, title=title)
+
     # Initialise the coordinator and save it as domain-data
     api = FrankEnergie(
         clientsession=async_get_clientsession(hass),
